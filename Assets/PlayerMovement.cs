3a29ec8308be5b8bcfd7da4fbdd710d21622d0e8
HanifLRZ <hanifonpc@gmail.com>
2025-07-24 14:04:44 +0700

    Movement System + Animation
    - Movement
    - Dodge
    - Animation

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("General")]
+    public float moveSpeed = 2;
+    public float maxSpeed = 10;
+    public float dodgeDistance = 80;
+    public float dodgeCooldownTime = 0.4f;
+    public float rotationSpeed = 720;
+    public Transform playerBody;
+    float targetRotY;
+    Rigidbody rb;
+    bool dodgeReady = true;
+    Vector2 moveDirection;
+    [Header("Animation")]
+    Animator anim;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = playerBody.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            moveDirection.x = Input.GetAxis("Horizontal");
+            moveDirection.y = Input.GetAxis("Vertical");
+
+            rb.AddForce(new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed, ForceMode.Force);
+            if (rb.linearVelocity.x > maxSpeed)
+            {
+                rb.linearVelocity = new Vector3(maxSpeed, rb.linearVelocity.y, rb.linearVelocity.z);
+            }
+            if (rb.linearVelocity.x < -maxSpeed)
+            {
+                rb.linearVelocity = new Vector3(-maxSpeed, rb.linearVelocity.y, rb.linearVelocity.z);
+            }
+            if (rb.linearVelocity.y > maxSpeed)
+            {
+                rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, maxSpeed);
+            }
+            if (rb.linearVelocity.y < -maxSpeed)
+            {
+                rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, -maxSpeed);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) && dodgeReady)
+            {
+                rb.linearDamping = 2;
+                if (anim != null)
+                {
+                    anim.SetTrigger("Roll");
+                }
+                StartCoroutine(DodgeCooldown());
+                Vector2 changedDirection = new Vector2();
+                if (moveDirection ==  Vector2.zero)
+                {
+                    changedDirection = playerBody.forward;
+                } else
+                {
+                    if (moveDirection.x != 0)
+                    {
+                        if (moveDirection.x > 0)
+                        {
+                            changedDirection = new Vector2(1, moveDirection.y);
+                        }
+                        else
+                        {
+                            changedDirection = new Vector2(-1, moveDirection.y);
+                        }
+                    }
+                    if (moveDirection.y != 0)
+                    {
+                        if (moveDirection.y > 0)
+                        {
+                            changedDirection = new Vector2(changedDirection.x, 1);
+                        }
+                        else
+                        {
+                            changedDirection = new Vector2(changedDirection.x, -1);
+                        }
+                    }
+                }
+                rb.AddForce(new Vector3(changedDirection.x, 0, changedDirection.y) * dodgeDistance, ForceMode.Impulse);
+            }
+
+            if (anim.GetCurrentAnimatorStateInfo(0).IsTag("Dodge") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f)
+            {
+                rb.linearDamping = 5f;
+            }
+
+            if (moveDirection != Vector2.zero)
+            {
+                Quaternion rotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.y), Vector3.up);
+                playerBody.rotation = Quaternion.RotateTowards(playerBody.rotation, rotation, rotationSpeed * Time.deltaTime);
+            }
+
+            IEnumerator DodgeCooldown()
+            {
+                dodgeReady = false;
+                yield return new WaitForSeconds(dodgeCooldownTime);
+                dodgeReady = true;
+            }
+
+            if (anim != null)
+            {
+                anim.SetBool("Moving", moveDirection != Vector2.zero);
+            }
+        }// Movement Calculation
+    }
+}
